671468caf0221da450f33442b4706f7d4bbbc321
kenfus <vincenzo.timmel@students.fhnw.ch>
2022-05-04 17:25:50 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# fhnw_ds_fs22_npr_mc2
+ Repo für die zweite Mini-Challenge des Moduls npr
---
+++ xlnet.py
@@ -0,0 +1,24 @@
+from transformers import Trainer, default_data_collator, AutoModelForQuestionAnswering, AutoTokenizer, TrainingArguments, pipeline
+from datasets import load_dataset, load_metric
+import torch
+import numpy as np
+from tqdm.auto import tqdm
+import collections
+
+class XLNET():
+
+    def __init__(self):
+        self.tokenizer = AutoTokenizer.from_pretrained("./test-squad-trained_xlnet")
+        self.model = AutoModelForQuestionAnswering.from_pretrained("./test-squad-trained_xlnet")
+
+    def predict(self, question: str, context: str):
+        """
+        Returns a dictionary containing score, start index , end index and answer
+        """
+        predictor = pipeline('question-answering', model=self.model, tokenizer=self.tokenizer)
+        return predictor({'question': question, 'context': context})
+
+
+if __name__ == '__main__':
+    testmodel = XLNET()
+    print(testmodel.predict("who am I?", "My name is Roman"))
